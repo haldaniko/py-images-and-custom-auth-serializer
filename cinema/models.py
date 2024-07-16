f1c7b86a3d6fb7bab8dcfaae10abd1aef280adee
haldaniko <haldaniko@gmail.com>
2024-07-16 03:21:25 +0200

    step 1

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,19 @@
+import os
+import uuid
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
+
+
+def movie_image_path(instance: "Movie", filename):
+    _, extension = os.path.splitext(filename)
+    unique_id = uuid.uuid4()
+    return os.path.join(
+        "uploads/images/",
+        f"{slugify(instance.title)}-{unique_id}{extension}"
+    )
 
 
 class CinemaHall(models.Model):
@@ -41,6 +54,8 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+
+    image = models.ImageField(null=True, upload_to=movie_image_path)
 
     class Meta:
         ordering = ["title"]
